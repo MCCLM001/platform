a1c93f820ad523a62c83adac4219c48b51616a0d
MCCLM001 <43081607+MCCLM001@users.noreply.github.com>
2020-06-26 17:57:33 -0700

    Add files via upload

---
+++ damage.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class damage : MonoBehaviour
+{
+    private int hp;
+    private bool invuln;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % 10 == 0 &&invuln == true)
+        {
+            hp -= 1;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "NME")
+        {
+            hp -= 1;
+            invuln = true;
+        }
+        
+    }
+    void OnCollisionExit2D(Collision2D c)
+    {
+        if (c.gameObject.tag == "NME")
+        {
+            invuln = false;
+        }
+    }
+}
---
+++ movement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float xspeed;
+    public float yspeed;
+    private Rigidbody2D rb;
+    private float mx = 0;
+    private float my = 0;
+    private bool gd = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && gd)
+        {
+            my = yspeed;
+            gd = false;
+        } else if(Input.GetKeyUp(KeyCode.Space) || !gd){
+            my = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            mx = -xspeed;
+        } else if (Input.GetKeyUp(KeyCode.A))
+        {
+            mx = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.D)){
+            mx = xspeed;
+        } else if (Input.GetKeyUp(KeyCode.D))
+        {
+            mx = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            my = -yspeed*2;
+        } else if (Input.GetKeyUp(KeyCode.S)) 
+        {
+            my = 0;
+        }
+
+        rb.velocity = new Vector2(mx, rb.velocity.y);
+        rb.AddForce (new Vector2(0, my));
+    }
+    void OnCollisionEnter2D(Collision2D c)
+    {
+        gd = true;
+    }
+}
